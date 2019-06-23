1d312ec2781abd3eb06fd7e5aeafe0146f9222ff
Anzhelica <anzhelica.protsenko@gmail.com>
2019-06-23 12:10:17 +0300

    todo react app

---
+++ src/components/item-status-filter/item-status-filter.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import './item-status-filter.css';
+
+export default class ItemStatusFilter extends Component {
+
+  constructor() {
+    super();
+
+    this.buttons = [
+      { name: 'all', label: 'All' },
+      { name: 'active', label: 'Active' },
+      { name: 'done', label: 'Done' }
+    ]
+  }
+  render() {
+    const { filter, onFilterChange } = this.props;
+
+    const buttons = this.buttons.map(({ name, label }) => {
+      const isActive = name === filter;
+      const classNames = 'btn ' + (isActive ? 'btn-info' : 'btn-outline-secondary');
+
+      return (
+        <button key={name}
+          type="button"
+          onClick={() => onFilterChange(name)}
+          className={classNames}>{label}</button>
+      );
+    });
+
+    return (
+      <div className="btn-group">
+        {buttons}
+      </div>
+    );
+  }
+}
+
